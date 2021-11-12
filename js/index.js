d484367f3fe4e2b9efd947c74ddfe835d6c0bd8f
Theo Sandell <theo01sandell@gmail.com>
2021-11-12 11:31:54 +0100

    Fixed header background on scroll

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove('scrolled');
+    }
+})
