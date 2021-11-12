f03558274b47f493526ac99c348ac7304e22a2ee
Theo Sandell <theo01sandell@gmail.com>
2021-11-12 11:56:53 +0100

    Removing anchor links from url

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 })
+
+//Remove anchor links (#) from url.
+window.addEventListener("DOMContentLoaded", function(e) {
+var links = document.getElementsByTagName("A");
+    for(var i=0; i < links.length; i++) {
+        if(!links[i].hash) continue;
+        if(links[i].origin + links[i].pathname != self.location.href) continue;
+        (function(anchorPoint) {
+            links[i].addEventListener("click", function(e) {
+                anchorPoint.scrollIntoView(true);
+                e.preventDefault();
+            }, false);
+        })(document.getElementById(links[i].hash.replace(/#/, "")));
+    }
+}, false);
