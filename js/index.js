8deaded73380e690cb669869aa43d98ba0744dbb
Theo Sandell <theo01sandell@gmail.com>
2021-11-12 15:52:30 +0100

    Created footer

--- js/index.js
+++ js/index.js
@@ -22,3 +22,11 @@ var links = document.getElementsByTagName("A");
         })(document.getElementById(links[i].hash.replace(/#/, "")));
     }
 }, false);
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+        type: "loop",
+        pagination: false
+    } );
+    splide.mount();
+} );
